6ecd3dc5cdb01d417f95517f6bfcf65bd205877b
Hiroki Yonetani <hirokiyonetani@gmail.com>
2024-06-30 20:31:37 +0200

    Refactoring

--- cmd/tf-module-deployer/deploy.go
+++ cmd/tf-module-deployer/deploy.go
@@ -13,29 +13,26 @@ func deployCommand() {
 		handleError("Error getting current directory", err)
 	}
 
-	// Create .tf-module directory if it doesn't exist
 	tfModuleDir := filepath.Join(cwd, ".tf-module")
-	if _, err := os.Stat(tfModuleDir); os.IsNotExist(err) {
-		if err := os.Mkdir(tfModuleDir, 0755); err != nil {
-			handleError("Error creating .tf-module directory", err)
-		}
+
+	// Create .tf-module directory if it doesn't exist
+	if err := createOrOverwriteDirectory(tfModuleDir); err != nil {
+		fmt.Printf("Error creating or overwriting .tf-module directory: %v\n", err)
+		os.Exit(1)
 	}
 
 	// Changing directory to .tf-module directory
 	if err := os.Chdir(tfModuleDir); err != nil {
 		handleError("Error changing directory to .tf-module", err)
 	}
-
 	fmt.Println("Changed directory to .tf-module")
 
-	// Move main.tf file to .tf-module directory
+	// Copying main.tf file to .tf-module directory
 	srcFile := filepath.Join(cwd, "main.tf")
 	dstFile := filepath.Join(tfModuleDir, "main.tf")
-
 	if err := copyFile(srcFile, dstFile); err != nil {
 		handleError("Error moving main.tf file", err)
 	}
-
 	fmt.Println("Moved main.tf to .tf-module directory")
 }
 
--- cmd/tf-module-deployer/file.go
+++ cmd/tf-module-deployer/file.go
@@ -6,6 +6,25 @@ import (
 	"os"
 )
 
+// createOrOverwriteDirectory creates a directory with the specified folderName in the current working directory (cwd).
+// If the directory already exists, it deletes all existing contents and recreates it.
+// It returns the absolute path to the newly created or overwritten directory.
+func createOrOverwriteDirectory(dirPath string) error {
+	// Remove directory if it exists
+	if _, err := os.Stat(dirPath); err == nil {
+		if err := os.RemoveAll(dirPath); err != nil {
+			return fmt.Errorf("failed to remove existing directory %s: %w", dirPath, err)
+		}
+	}
+
+	// Create new directory
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		return fmt.Errorf("failed to create directory %s: %w", dirPath, err)
+	}
+
+	return nil
+}
+
 func copyFile(src, dst string) error {
 	source, err := os.Open(src)
 	if err != nil {
