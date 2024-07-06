66f7b03752e55b4daac0c7cac5b5ab0e4b2e770b
Hiroki Yonetani <hirokiyonetani@gmail.com>
2024-07-06 21:22:05 +0200

    Add terraform plan

---
+++ cmd/plan.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"tf-module-deployer/utils"
+
+	"github.com/spf13/cobra"
+)
+
+var planCmd = &cobra.Command{
+    Use:   "plan",
+    Short: "Run terraform plan",
+    Long:  `This command runs terraform plan in the .tf-module-deployer/examples directory.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		planCommand()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(planCmd)
+}
+
+func planCommand() {
+	// Run terraform plan
+	err := utils.RunTerraformCommand("plan", ModuleDir)
+	utils.HandleError(
+		fmt.Sprintf("Failed to run terraform plan in %s", ModuleDir), err)
+}
+
